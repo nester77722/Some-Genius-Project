c7ec7b7328543ce152e7c7fb0da6a380259e8f95
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2022-09-04 21:04:46 +0300

    Added api publish

--- BoardGames.Api/BoardGames.API/Startup.cs
+++ BoardGames.Api/BoardGames.API/Startup.cs
@@ -68,10 +68,12 @@ namespace BoardGames.API
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+
             }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
 
             //app.UseHttpsRedirection();
 
