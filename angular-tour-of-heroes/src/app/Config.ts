02832b79da065eb3b029610db18ff94d9205f6af
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2022-09-04 21:03:49 +0300

    Removed unused angular components

--- angular-tour-of-heroes/src/app/Config.ts
+++ angular-tour-of-heroes/src/app/Config.ts
@@ -1,3 +1,3 @@
 export const Config = {
-   apiUrl: 'http://localhost:5202'
+   apiUrl: 'http://192.168.0.104:8082'
 }
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GenresComponent } from './genres/genres.component';
 import { GenresDetailComponent } from './genres-detail/genres-detail.component';
@@ -15,10 +12,7 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent,
     GenresComponent,
     GenresDetailComponent,
     MechanicsComponent,
